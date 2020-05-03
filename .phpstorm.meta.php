67c5ab47e2f88c95f4471f49d86eda68e0b0a6fc
Jordan Kniest <mail@jkniest.de>
2020-05-04 00:09:41 +0200

    Add phpstorm meta file for auto completion

---
+++ .phpstorm.meta.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PHPSTORM_META {
+    expectedArguments(
+        \jkniest\HueIt\Light::setEffect(),
+        0,
+        'none',
+        'colorloop'
+    );
+
+    expectedArguments(
+        \jkniest\HueIt\Light::setAlert(),
+        0,
+        'none',
+        'select',
+        'lselect'
+    );
+}
